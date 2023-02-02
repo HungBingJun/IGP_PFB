81f74d37059ff9f4a46a4b5c0e381170d6725d82
HungBingJun <s10243169@connect.np.edu.sg>
2023-02-02 09:56:46 +0800

    Cash on hand pt 2

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -19,5 +19,24 @@ def cash_on_hand():
     -No parameters are required
     """
     #creating an empty list to store negative differences
-    negative = []        
+    negative = []
     
+    # for loop to loop through the rows (1 to the numnber of sublist in data_list)
+    for row in range(1,len(data_list)):
+        # find the difference between cash on hand on the day and the cash on hand on the day before
+        difference = (int(data_list[row][1])) - int(data_list[row-1][1])
+        # when difference is negative, the day and the difference amount would be extended in the negative list
+        if difference < 0:
+            negative.extend([data_list[row][0],difference])
+        # when difference is positive, the program will continue and the positive value will not be assigned to anything
+        else:
+            continue
+    # if there is no values in negative list, program will return cash surplus
+    if len(negative) == 0:
+        return("[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN PREVIOUS DAY")
+    
+    # else it will return the cash defecit of the day itself and amount in positive numbers
+    else:
+        # the category will be even indexes will be category while the odd indexes are the values.
+        for index in range(0,len(negative),2):
+            return(f"[CASH DEFECIT] DAY: {float(negative[index])}, AMOUNT: USD{abs(negative[index +1])}")
