f86a2a90375e7bfea51acdfcc7c3a8e9848b6e4e
HengZengXiang-22 <s10245516@connect.np.edu.sg>
2023-02-02 15:35:57 +0800

    done

--- profit_loss.py
+++ profit_loss.py
@@ -4,11 +4,14 @@ fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 
 #reading mode to read csv file
 with fp.open(mode = "r",encoding = "UTF-8", newline ="") as file:
+    
     #instantiate a reader object
     reader = csv.reader(file)
     next(reader)
+    
     #creating empty data_list to store all the raw data
     data_list = []
+    
     #appending each line from reader to data_list
     for line in reader:
         data_list.append(line)
